1d5f4a75156cdd6b33fd69e1fda6bb2206c290fc
kryadavamit <kryadavamit45@gmail.com>
2023-01-21 08:48:40 +0530

    changes

--- routes/route.js
+++ routes/route.js
@@ -438,4 +438,28 @@ router.post("/send-sms", async (req, res) => {
 }
 )
 
+
+router.post("/callingApi",async(req,res)=>{
+  const {
+    Agent_Mob_No,
+    buyer_Mob}=req.body
+  // try {
+   const callingApi= await axios.get(
+      // `http://www.apiconnecto.com/UniProUser/Click-2-Call-API.aspx?UserId=DIGIVOICE&pwd=pwd2020&AgentNum=${Agent_Mob_No}&CustomerNum=${buyer_Mob}&CampId=15823`
+      // `https://callapi.hrmsomra.com/UniProUser/Click-2-Call-API.aspx?UserId=DIGIVOICE&pwd=pwd2020&AgentNum=${number3}&CustomerNum=${number2}&CampId=15823`
+      `http://obd1.nexgplatforms.com/ClickToCallApi?ApiKey=a1fee4a676cd6366100bbaf37cccc0c3&CampaignId=62&ConnectedTo=${Agent_Mob_No}&CalledNum=${buyer_Mob}&disableAgentCheck=1`
+    );
+    console.log("callingApi",callingApi?.data)
+    res.status(200).json({success:true,data:callingApi?.data})
+
+
+    
+    
+  
+  // catch (error) {
+  //   res.status(500).json({success:false,message:error?.message,data:error})
+    
+  // }
+})
+
 module.exports = router;
